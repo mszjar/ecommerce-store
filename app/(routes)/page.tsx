8679ce9aad6b2737e7cae4af729dbf355962bb9a
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-09-12 21:20:12 +0200

    actions and modals

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("19ff3fff-c967-4329-a5d0-c646d3ec0062");
 
   return (
     <Container>
