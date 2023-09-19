47eff7a77c75cac672f8e0617e1c52c1109fe899
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-09-19 09:43:24 +0200

    logo added

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
 import NavbarActions from "@/components/navbar-actions";
 import getCategories from "@/actions/get-categories";
+import Image from "next/image";
 
 const Navbar = async () => {
   const categories = await getCategories();
@@ -13,7 +14,13 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">Buenled</p>
+            <Image
+              src="/images/logo.png"
+              alt="Buenled"
+              width={140}
+              height={140}
+            >
+            </Image>
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
